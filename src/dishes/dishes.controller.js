5938995f3cfb3a1bfeaf796d87b440fe31de56d8
gagegraves <gagegraves97+github@gmail.com>
2021-09-01 11:12:46 -0700

    all tests passing, first submission

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -8,8 +8,60 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
 
-function dishExists(req, res, next) {
-  const dishId = Number(req.params.dishId);
+function listDishes(req, res) {
+  res.json({ data: dishes });
+}
+
+function createDish(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    name: name,
+    description: description,
+    price: price,
+    image_url: image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function validateDishBody(req, res, next) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  
+  if (!name || name === "") {
+    return next({
+      status: 400,
+      message: "Dish must include a name",
+    });
+  } else if (!description || description === "") {
+    return next({
+      status: 400,
+      message: "Dish must include a description",
+    });
+  } else if (!price) {
+    return next({
+      status: 400,
+      message: "Dish must include a price."
+    })
+  } else if (price <= 0 || !Number.isInteger(price)) {
+    return next({
+      status: 400,
+      message: "Dish must have a price that is an integer greater than 0",
+    });
+  } else if (!image_url || image_url === "") {
+    return next({
+      status: 400,
+      message: "Dish must include a image_url",
+    });
+  } else next();
+}
+
+function getDish(req, res) {
+  res.json({ data: res.locals.dish });
+}
+
+function validateDishId(req, res, next) {
+  const dishId = req.params.dishId;
   const foundDish = dishes.find((dish) => dish.id === dishId);
   if (foundDish) {
     res.locals.dish = foundDish;
@@ -17,55 +69,48 @@ function dishExists(req, res, next) {
   }
   next({
     status: 404,
-    message: `URL not found: ${req.originalUrl}`,
+    message: `Dish id does not exist: ${dishId}`,
   });
 }
 
-function bodyHasRequiredProperties(req, res, next) {
+function updateDish(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
 
-  const { data: { name } = {} } = req.body;
-  const { data: { description } = {} } = req.body;
-  const { data: { price } = {} } = req.body;
-  const { data: { image_url } = {} } = req.body;
-
-  if (!name) {
-    next({
-      status: 400,
-      message: "Dish must include a name",
-    });
-  } else if (!description) {
-    next({
-      status: 400,
-      message: "Dish must include a description",
-    });
-  } else if (!price) {
-    next({
-      status: 400,
-      message: "Dish must have a price that is an integer greater than 0",
-    });
-  } else if (!image_url) {
-    next({
-      status: 400,
-      message: "Dish must include a image_url",
-    });
-  } else next();
-}
-
-function list(req, res) {
-  res.json({ data: dishes });
-}
-
-function create(req, res) {
-  const { data: { name } = {} } = req.body;
-  const newUrl = {
-    id: ++lastUrlId,
-    href,
+  res.locals.dish = {
+    id: res.locals.dishId,
+    name: name,
+    description: description,
+    price: price,
+    image_url: image_url,
   };
-  urls.push(newUrl);
-  res.status(201).json({ data: newUrl });
+
+  res.json({ data: res.locals.dish });
 }
+
+function validateDishBodyId(req, res, next) {
+  const { dishId } = req.params;
+  const { data: { id } = {} } = req.body;
+
+  if (!id || id === dishId) {
+    res.locals.dishId = dishId;
+    return next();
+  }
+
+  next({
+    status: 400,
+    message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+  });
+}
+
 
 module.exports = {
-  list,
-  create: [bodyHasRequiredProperties, create],
+  listDishes,
+  createDish: [validateDishBody, createDish],
+  getDish: [validateDishId, getDish],
+  updateDish: [
+    validateDishId,
+    validateDishBody,
+    validateDishBodyId,
+    updateDish,
+  ],
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
 const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed")
+
 // TODO: Implement the /dishes routes needed to make the tests pass
 
 router
 .route('/')
-.get(controller.list)
-.post(controller.create)
+.get(controller.listDishes)
+.post(controller.createDish)
 .all(methodNotAllowed);
 
+router
+.route('/:dishId')
+.get(controller.getDish)
+.put(controller.updateDish)
+.all(methodNotAllowed);
 
 module.exports = router;
